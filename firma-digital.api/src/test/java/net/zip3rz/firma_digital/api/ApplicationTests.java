9d9ea2e3b213131a987d3d0baa24cb8365ce2e15
ziP3Rz <94759675+ziP3Rz@users.noreply.github.com>
2024-10-23 16:25:11 +0200

    Proyecto inicializado con SpringInitializr
    Se crea el proyecto a través de la web https://start.spring.io/
    
    Características generales:
    - Maven
    - Java 17
    - Empaquetado Jar
    
    Dependencias iniciales:
    - Spring Web
    - Lombok
    - Spring Data JPA
    - PostgreSQL Driver

---
+++ firma-digital.api/src/test/java/net/zip3rz/firma_digital/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.zip3rz.firma_digital.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
